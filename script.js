08245d7aa10caa0ed0e524cc7d964aa963a0a627
mathfreitas03 <116849424+mathfreitas03@users.noreply.github.com>
2023-03-16 23:00:37 -0300

    2.0

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 window.sr = ScrollReveal({ reset: true });
 
 sr.reveal('#openingelem', { duration: 2000 });
+
+const seta = document.querySelector('#seta')
+
+seta.addEventListener('click', () =>{
+    window.scroll({top: window.outerHeight, behavior: "smooth"})
+})
